56c347f09f454c4d4b392015523789f52dd51817
ashraf <ashraf1abdelrasool@gmail.com>
2019-01-12 16:46:56 +0200

    push all

--- src/main/java/org/revo/Config/SecurityConfig.java
+++ src/main/java/org/revo/Config/SecurityConfig.java
@@ -1,54 +1,25 @@
 package org.revo.Config;
 
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
  * Created by ashraf on 18/04/17.
  */
-@EnableResourceServer
-@Configuration
-public class SecurityConfig extends ResourceServerConfigurerAdapter {
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .anonymous()
-                .and().authorizeRequests()
-                .anyRequest().authenticated();
-    }
-
+@EnableWebFluxSecurity
+public class SecurityConfig {
     @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange()
+                .matchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt().and().and().build();
     }
 
-    @Override
-    public void configure(ResourceServerSecurityConfigurer config) {
-        config.tokenServices(tokenServices());
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("123");
-        return converter;
-    }
 }
--- src/main/java/org/revo/Service/Impl/UserServiceImpl.java
+++ src/main/java/org/revo/Service/Impl/UserServiceImpl.java
@@ -1,16 +1,8 @@
 package org.revo.Service.Impl;
 
 import org.revo.Service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -18,10 +10,11 @@ import java.util.Optional;
  */
 @Service
 public class UserServiceImpl implements UserService {
-    @Autowired
-    private TokenStore tokenStore;
+//    @Autowired
+//    private TokenStore tokenStore;
     @Override
     public Optional<String> current() {
+/*
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null) {
             if (authentication instanceof OAuth2Authentication) {
@@ -32,6 +25,7 @@ public class UserServiceImpl implements UserService {
                 return Optional.of("-1");
             } else return Optional.of("-1");
         }
+*/
         return Optional.of("-1");
     }
 }
